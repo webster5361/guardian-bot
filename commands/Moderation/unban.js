0343d0338429f20f5c39f0c0f514317d368f633b
Odinthewanderer <webster5361@gmail.com>
2017-07-20 21:51:15 -0500

    Updated DB integration... still WIP

--- commands/Moderation/unban.js
+++ commands/Moderation/unban.js
@@ -1,11 +1,16 @@
 exports.run = (client, msg, [member, ...reason]) => {
   reason = reason.join(' ');
 
-  if (!reason) return msg.reply('You must supply a reason for the kick!');
-
-  if (!member) return msg.reply('You must supply a User Resolvable, such as a user id.').catch(console.error);
-  msg.guild.unban(member);
-  msg.channel.send('Unban successful!');
+  let caseNumber = 0;
+  var casenum = client.funcs.db_getCaseNum(msg.guild.id, function(data) {
+    caseNumber = data;
+  });
+  console.log(caseNumber);
+  // if (!reason) return msg.reply('You must supply a reason for the kick!');
+  //
+  // if (!member) return msg.reply('You must supply a User Resolvable, such as a user id.').catch(console.error);
+  // msg.guild.unban(member);
+  // msg.channel.send('Unban successful!');
   return 0;
 };
 
